f5227d88902bb54fc3f951a55ad3be21827dccd6
dheeraj <dheeraj.com>
2020-03-01 17:51:34 +0530

    added to the login api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+#from rest_framework.authtoken.models  import Token
 
 
 class UserProfileManager(BaseUserManager):
